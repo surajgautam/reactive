9972f0829cf153247c88755884d35696b5d5a7d4
Suraj Gautam <teamwarrior200@gmail.com>
2022-08-14 19:31:33 -0500

    feat: add repository

--- src/main/java/com/surajgautam/reactive/handlers/StudentHandler.java
+++ src/main/java/com/surajgautam/reactive/handlers/StudentHandler.java
@@ -1,20 +1,22 @@
 package com.surajgautam.reactive.handlers;
 
 import com.surajgautam.reactive.model.Student;
+import com.surajgautam.reactive.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 @Component
+@RequiredArgsConstructor
 public class StudentHandler {
+
+    private final StudentRepository repository;
+
     public Mono<ServerResponse> getStudentById(final Long id) {
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(getStudent(id), Student.class);
-    }
-
-    private Mono<Student> getStudent(final Long id) {
-        return Mono.just(new Student(id, "Suraj Gautam", "UTA Dallas"));
+                .body(this.repository.findById(id).log(), Student.class);
     }
 }
---
+++ src/main/java/com/surajgautam/reactive/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.surajgautam.reactive.repository;
+
+import com.surajgautam.reactive.model.Student;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface StudentRepository extends ReactiveMongoRepository<Student, Long> {
+
+}
